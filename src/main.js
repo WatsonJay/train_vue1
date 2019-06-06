84d33b09b132722957effd5db8af7b71aae7fa57
WatsonJay <446725026@qq.com>
2019-06-06 11:27:08 +0800

    vue-cli

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+//引入elemen-u控件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.config.productionTip = false
+//使用use命令后才起作用
+Vue.use(ElementUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
